9bba4062d4863e3cbe9c204c9ffea7cf37e35979
1061568-art <1061568@bkrnet.de>
2026-04-16 09:12:34 +0200

    MySQL

--- Daba_reservationApp/Program.cs
+++ Daba_reservationApp/Program.cs
@@ -9,19 +9,21 @@ namespace Daba_reservationApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-            builder.Services.AddDbContext<ApplicationDbContext>(
-                options =>
-                {
-                    options.UseInMemoryDatabase("ReservationsDb");
-                }
-            );
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+
+  
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
